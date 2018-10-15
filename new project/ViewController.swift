3a1f1381d220ec34d3d0307d4cbfdacad3c90de2
Гость <Guest@mbp008.local>
2018-10-15 15:59:48 +0300

    Initial Commit

---
+++ new project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  new project
+//
+//  Created by Гость on 15/10/2018.
+//  Copyright © 2018 Гость. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
